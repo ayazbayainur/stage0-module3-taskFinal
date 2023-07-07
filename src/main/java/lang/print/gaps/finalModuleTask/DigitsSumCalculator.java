6d56ec87abb18e6fc38f9d72c64729d3ab51fdb6
Ainur <ayazbay.ainur@mail.ru>
2023-07-08 00:31:43 +0600

    2nd commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,12 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        int sum = 0;
+        int ans = 0;
         while(number > 0){
-            sum += number % 10;
+            ans += number % 10;
             number /= 10;
         }
-        System.out.println(sum);
+        System.out.println(ans);
     }
 
     public static void main(String[] args){
